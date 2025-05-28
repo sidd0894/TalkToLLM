23c3bc7c0444b349fc93b45315b0086ca2f8059f
sidd0894 <112334974+sidd0894@users.noreply.github.com>
2025-05-28 22:12:27 +0530

    added model name parameter

--- voice_llm.py
+++ voice_llm.py
@@ -1,21 +1,18 @@
 from ollama import chat
 from speech_utils.speech_converter import SpeechToSpeech
 
+model = input('Ollama model name: ').strip()
 system_prompt = input('System Prompt: ').strip()
-# systemPrompt = "Talk like a human. Do not answer too much long. Do not use any emojis. Do not answer in multiple paragraphs. Do not use asterisk anywhere in the answer."
-# systemPrompt = "Your name is Mark. Talk like a human. Do not respond too much long. Do not use any emojis. Do not respond in bullet points or points. Do not use asterisk anywhere in the response."
-
 conversation_context = [{"role": "system", "content": system_prompt}]
 
 
-def generateResponse(prompt):
+def generateResponse(prompt, model_name):
     global conversation_context
 
     userMessage = {"role": "user", "content": prompt}
     conversation_context.append(userMessage)
 
-    # response = chat(model="gemma3:1b", messages=conversation_context)
-    response = chat(model="llama3.2:1b", messages=conversation_context)
+    response = chat(model=model_name, messages=conversation_context)
     reply = response.message.content.strip().replace('*', '')
     modelMesage = {"role": "assistant", "content": reply}
     conversation_context.append(modelMesage)
@@ -32,7 +29,7 @@ def main():
             break
 
         print(f'Prompt: {user_prompt}')
-        response = generateResponse(prompt=user_prompt)
+        response = generateResponse(prompt=user_prompt, model_name=model)
         print(f'Response: {response}')
         speech.text_to_speech(response, speech_rate=145, voice_index=1)
 
